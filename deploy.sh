ab3aca93449d170be4e150821f5a4b694940dfee
James Logan <james@dbej.net>
2024-05-22 11:13:25 -0400

    Updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-ng build --base-href "https://JimJebadiah.github.io/5e-psc/"
-ng deploy --base-href "https://JimJebadiah.github.io/5e-psc/" --dir=dist/psc
-# ngh --dir=dist/psc
+ng build --base-href "https://JimJebadiah.github.io/5e-character-sheet/"
+ng deploy --base-href "https://JimJebadiah.github.io/5e-character-sheet/" --dir=dist/5e-character-sheet
